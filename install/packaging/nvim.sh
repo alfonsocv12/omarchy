6d5f5ecdd148adfae9d48288dff5ccdce49fc840
Alfonso Villalobos Ulate <alfonsocvu@gmail.com>
2026-03-15 13:21:45 -0600

    Not sure about this deletions trying to avoid halts for now

--- install/packaging/nvim.sh
+++ install/packaging/nvim.sh
@@ -1,2 +1,6 @@
 # Includes lazyvim and the themes
-omarchy-nvim-setup
+if command -v omarchy-nvim-setup &>/dev/null; then
+  omarchy-nvim-setup
+else
+  echo "omarchy-nvim tool not found, skipping nvim setup."
+fi

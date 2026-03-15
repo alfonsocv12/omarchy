52c7157905de8dc5d2ff504cadbf8ced418dd04d
Alfonso Villalobos Ulate <alfonsocvu@gmail.com>
2026-03-15 13:49:49 -0600

    Add mise repo

--- install/preflight/apt.sh
+++ install/preflight/apt.sh
@@ -1,6 +1,11 @@
 if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
   # Install build tools
-  omarchy-pkg-add build-essential
+  omarchy-pkg-add build-essential curl gpg
+
+  # Configure apt repositories (Mise)
+  sudo install -dm 755 /etc/apt/keyrings
+  curl -fsSL https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list >/dev/null
 
   # Configure apt
   sudo DEBIAN_FRONTEND=noninteractive apt-get update

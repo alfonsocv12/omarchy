e92e0670c362adec0d80412cd24587019084cab0
Alfonso Villalobos Ulate <alfonso@Alfonsos-MacBook-Pro.local>
2026-03-16 22:07:02 -0600

    arch base on host

--- install/preflight/apt.sh
+++ install/preflight/apt.sh
@@ -5,7 +5,7 @@ if [[ -n ${OMARCHY_ONLINE_INSTALL:-} ]]; then
   # Configure apt repositories (Mise)
   sudo install -dm 755 /etc/apt/keyrings
   curl -fsSL https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1>/dev/null
-  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list >/dev/null
+  echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=$(dpkg --print-architecture)] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list >/dev/null
 
   # Configure apt
   sudo DEBIAN_FRONTEND=noninteractive apt-get update
